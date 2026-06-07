9988b4ea77925aad1a494c4fbdaa6970e2847798
Rishi Dixit <rishi.dixit129@gmail.com>
2026-06-07 16:44:48 +0200

    board adding page added

--- app/app/src/main/java/com/example/taskman/AppAddPage.kt
+++ app/app/src/main/java/com/example/taskman/AppAddPage.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,31 +15,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
+import androidx.compose.material3.AlertDialogDefaults.containerColor
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
 
-class AppHomePage : ComponentActivity() {
+class AppAddPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var searchQuery by remember { mutableStateOf("") }
+            var boardName by remember { mutableStateOf("") }
 
             Column(
                 modifier = Modifier
@@ -50,29 +54,27 @@ class AppHomePage : ComponentActivity() {
                 Spacer(modifier = Modifier.height(30.dp))
 
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
-                        text = "Welcome Back!",
-                        color = Color.White,
-                        fontSize = 30.sp,
-                        modifier = Modifier
-                            .offset(x = 20.dp)
-                            .weight(1f)
-                    )
+                    ClickableExitIcon()
 
-                    ClickableAddIcon()
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    CreateButton()
                 }
+                Spacer(modifier = Modifier.height(40.dp))
+
                 Column(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    SearchBar(
-                        labText = "Search",
-                        value = searchQuery,
-                        icon = R.drawable.search,
-                        onValueChange = { searchQuery = it },
+                    AddTextField(
+                        labText = "Name",
+                        value = boardName,
+                        onValueChange = { boardName = it },
                         modifier = Modifier.fillMaxWidth(0.9f)
                     )
                 }
@@ -81,8 +83,9 @@ class AppHomePage : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun SearchBar(
+fun AddTextField(
     labText: String,
     value: String,
     icon: Int? = null,
@@ -94,39 +97,46 @@ fun SearchBar(
         onValueChange = onValueChange,
         label = { Text(labText) },
         modifier = modifier,
-        shape = RoundedCornerShape(15.dp),
+        shape = RoundedCornerShape(25.dp),
         colors = TextFieldDefaults.colors(
-            focusedContainerColor = Color(0xFF828282),
-            unfocusedContainerColor = Color(0xFF828282),
+            focusedContainerColor = Color(0xFF404041),
+            unfocusedContainerColor = Color(0xFF404041),
             focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedLabelColor = Color(0xFF727272),
+            unfocusedLabelColor = Color(0xFF727272)
         ),
         textStyle = TextStyle(fontSize = 14.sp),
         singleLine = true,
-        leadingIcon = icon?.let {
-            {
-                Image(
-                    painter = painterResource(id = it),
-                    contentDescription = null,
-                    modifier = Modifier.size(24.dp)
-                )
-            }
-        }
     )
 }
 
 @Composable
-fun ClickableAddIcon() {
+fun ClickableExitIcon() {
     val context = LocalContext.current
 
     Image(
-        painter = painterResource(id = R.drawable.add),
-        contentDescription = "Dodaj nową tablicę",
+        painter = painterResource(id = R.drawable.exit),
+        contentDescription = "Wróć",
         modifier = Modifier
-            .size(120.dp)
+            .size(40.dp)
             .clickable {
-                val intent = Intent(context, AppAddPage::class.java)
+                val intent = Intent(context, AppHomePage::class.java)
                 context.startActivity(intent)
             }
     )
+}
+
+@Composable
+fun CreateButton() {
+    Button(onClick = {
+        println("Przycisk został kliknięty!")
+    },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF404041),
+            contentColor = Color.White
+    ),
+    shape = RoundedCornerShape(25.dp)) {
+        Text(text = "Create")
+    }
 }
